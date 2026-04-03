1a3c83b0e125665c1fef278b9e1360f8ed75586f
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-04-03 20:51:30 +0700

    feat: most comprehensive rules from all projects, updated clean.sh

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-rm -rf node_modules bun.lock .cache .turbo **/node_modules **/.cache **/.next **/.turbo **/dist
+rm -rf node_modules bun.lock .cache .turbo **/node_modules **/.cache **/.next **/.turbo **/dist **/build **/tsconfig.tsbuildinfo **/test-results
