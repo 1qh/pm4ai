2a31a114a365411225e9e352e6cc233663496973
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-04-05 16:28:15 +0700

    feat: add --version flag

--- packages/pm4ai/src/cli.ts
+++ packages/pm4ai/src/cli.ts
@@ -1,11 +1,13 @@
 /* eslint-disable no-console */
+import pkg from '../package.json' with { type: 'json' }
 import { guide } from './guide.js'
 import { preflight } from './preflight.js'
 import { setVerbose } from './utils.js'
 const command = process.argv[2]
 const flags = new Set(process.argv.slice(3))
 if (flags.has('--verbose')) setVerbose(true)
-if (!command) console.log(guide)
+if (command === '--version' || command === '-v') console.log(pkg.version)
+else if (!command) console.log(guide)
 else if (command === 'init') {
   const name = process.argv[3]
   if (name) {
